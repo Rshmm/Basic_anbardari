4fb5663533e82ef2f1da385fdc3e24cd8ee9abf5
arsham <arshsm.hajeb@gamil.com>
2024-07-31 23:00:38 +0330

    تست کردن جدول db

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 import mysql.connector
 
+
+# Connect to db
 db = mysql.connector.connect(user="root", password="root123", database="warehouse", host="localhost")
+curser = db.cursor()
+
+# Oprations
+curser.execute("INSERT INTO `product` (`name`, `brand`, `price`, `count`) VALUES ('phone' , 'samsung', '1700', '12')")
+
+
+# Save
+db.commit()
+
+
+# Disconnect
+curser.close()
+db.close()
